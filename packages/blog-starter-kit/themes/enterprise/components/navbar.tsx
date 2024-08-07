16ae47e5353ce531908c706f0aa9bdc49f89744f
paidre <150460610+paidre@users.noreply.github.com>
2024-08-08 02:31:09 +0800

    Update navbar.tsx

--- packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
@@ -5,6 +5,7 @@ export const Navbar = () => {
 	return (
 		<div className="grid grid-cols-1 items-center gap-5 pt-5 text-sm md:grid-cols-2">
 			<Search />
+			
 			{/* <SocialLinks /> */}
 		</div>
 	);
