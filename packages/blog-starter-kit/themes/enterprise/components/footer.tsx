8f28245850189810d95b51576f5a40a58e3c8f40
paidre <150460610+paidre@users.noreply.github.com>
2024-08-10 05:24:34 +0800

    Update footer.tsx

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -17,16 +17,16 @@ export const Footer = () => {
       <div className="mx-auto my-auto w-full max-w-screen-xl items-center justify-between px-4">
         <div className="mb-10 grid gap-10 md:grid-cols-6">
           <div className="my-4 grid auto-rows-min gap-4 col-span-2">
-            {/* <Link className="hover:text-[#0fa0fa] text-[#555]" href="/" prefetch={false}>
+            {/* <a className="hover:text-[#0fa0fa] text-[#555]" href="/" >
 				  <IconPaidreUnpadded className="w-[88px] md:w-[88px]" />
-				</Link> */}
+				</a> */}
 
-            <Link href={windowOrigin + "/"} prefetch={false}>
+            <a href={windowOrigin + "/"} >
               <span className="flex items-center justify-start text-[24px] font-black">
                 <IconPaidreUnpadded className="w-10 mr-2 md:mr-4" />
                 Paidre
               </span>
-            </Link>
+            </a>
             {/* <div className="my-4 text-[#888] text-[16px] md:text-[14px] max-w-[300px]">Resolve late payments online, before lawyers or collectors.</div> */}
             {/* <div className="my-4 text-[#888] text-[16px] max-w-[300px]">Accounts Receivable & Credit Control platform for aging debts</div> */}
             <p className="my-4 text-[#888] text-[16px] max-w-[300px]">Late payment platform for creditors to manage aging debtors on the internet.</p>
@@ -35,80 +35,80 @@ export const Footer = () => {
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Product</h6>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} target="_blank" prefetch={false}>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} target="_blank" >
               Submit a claim
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} >
               Bulk claims upload
-            </Link>
+            </a>
             <h6 className="font-[600] mt-4">Cases</h6>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} prefetch={false}>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} >
               Overdue invoices
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} >
               Default on loans
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} >
               Outstanding salaries
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/"} >
               Unprocessed refunds
-            </Link>
+            </a>
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Solutions</h6>
 
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" prefetch={false}>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" >
               Business debtors
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" >
               Individual debtors
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" >
               Business creditors
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href="/" target="_blank" >
               Individual creditors
-            </Link>
+            </a>
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Company</h6>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/about-us"} target="_blank" prefetch={false}>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/about-us"} target="_blank" >
               About us
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/contact-us"} target="_blank" rel="nofollow">
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/contact-us"} target="_blank" rel="nofollow">
               Contact us
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/blog"} target="_blank" rel="nofollow">
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/blog"} target="_blank" rel="nofollow">
               Blog
-            </Link>
+            </a>
 
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href="https://linkedin.com/company/paidre" target="_blank" rel="nofollow">
+            <a className="hover:text-[#0fa0fa] text-[#555]" href="https://linkedin.com/company/paidre" target="_blank" rel="nofollow">
               LinkedIn
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href="https://facebook.com/paidredotcom" target="_blank" rel="nofollow">
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href="https://facebook.com/paidredotcom" target="_blank" rel="nofollow">
               Facebook
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href="https://paidre.betteruptime.com" target="_blank" rel="nofollow">
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href="https://paidre.betteruptime.com" target="_blank" rel="nofollow">
               System status
-            </Link>
+            </a>
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Legal</h6>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/privacy-policy"} target="_blank" rel="nofollow" prefetch={false}>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/privacy-policy"} target="_blank" rel="nofollow" >
               Privacy policy
-            </Link>
-            <Link className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/terms-of-use"} target="_blank" rel="nofollow" prefetch={false}>
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555]" href={windowOrigin + "/terms-of-use"} target="_blank" rel="nofollow" >
               Terms of use
-            </Link>
-            {/* <Link className="hover:text-[#0fa0fa] text-[#555]"
+            </a>
+            {/* <a className="hover:text-[#0fa0fa] text-[#555]"
 				  href="/community-guidelines"
 				  target="_blank"
 				  rel="nofollow"
-				  prefetch={false}
+				  
 				>
 				  Community Guidelines
-				</Link> */}
+				</a> */}
           </div>
         </div>
 
