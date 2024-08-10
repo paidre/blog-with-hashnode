9c8c5fd798a34f3550e51d9af209b530868a3fd8
paidre <150460610+paidre@users.noreply.github.com>
2024-08-10 15:01:21 +0800

    Update footer.tsx

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -1,110 +1,93 @@
 import { IconPaidreUnpadded } from "../assets/icon-paidre-unpadded";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-
 
 export const Footer = () => {
   const currentTime = new Date();
   const year = currentTime.getFullYear();
-//   const [windowOrigin, setWindowOrigin] = useState('/');
 
-
-//   useEffect(() => {
-//     typeof window !== undefined && setWindowOrigin(window?.location?.origin);
-//   }, []);
-
-  // console.log("apple",window?.location.origin)
   return (
     <footer className="flex bg-gradient-to-r from-[#f8f8f8] to-white border-t border-[#ddd] w-full bg-[#fff] pt-20 pb-[16px]">
       <div className="mx-auto my-auto w-full max-w-screen-xl items-center justify-between px-4">
         <div className="mb-10 grid gap-10 md:grid-cols-6">
-          <div className="my-4 grid auto-rows-min gap-4 col-span-2">
-            {/*<div className="hover:text-[#0fa0fa] text-[#555]" href="/" >
-				  <IconPaidreUnpadded className="w-[88px] md:w-[88px]" />
-				</Link> */}
-
-           <div onClick={()=>window.open(window?.location?.origin)} >
+          <div className="my-4 grid auto-rows-min gap-4 md:col-span-2">
+            <div onClick={() => window.open(window?.location?.origin)}>
               <span className="flex items-center justify-start text-[24px] font-black">
                 <IconPaidreUnpadded className="w-10 mr-2 md:mr-4" />
                 Paidre
               </span>
-           </div>
-            {/* <div className="my-4 text-[#888] text-[16px] md:text-[14px] max-w-[300px]">Resolve late payments online, before lawyers or collectors.</div> */}
-            {/* <div className="my-4 text-[#888] text-[16px] max-w-[300px]">Accounts Receivable & Credit Control platform for aging debts</div> */}
+            </div>
+
             <p className="my-4 text-[#888] text-[16px] max-w-[300px]">Late payment platform for creditors to manage aging debtors on the internet.</p>
 
             <div className="my-10"></div>
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Product</h6>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Submit a claim
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Bulk claims upload
-           </div>
+            </div>
             <h6 className="font-[600] mt-4">Cases</h6>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Overdue invoices
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Default on loans
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Outstanding salaries
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Unprocessed refunds
-           </div>
+            </div>
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Solutions</h6>
 
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Business debtors
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Individual debtors
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Business creditors
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/`)} >
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/`)}>
               Individual creditors
-           </div>
+            </div>
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Company</h6>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/about-us`)} >
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/about-us`)}>
               About us
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/contact-us`)}>
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/contact-us`)}>
               Contact us
-           </div>
-           <div className="hover:text-[#0fa0fa] text-[#555]" onClick={()=>window.open(`${window?.location?.origin}/blog`)}>
+            </div>
+            <div className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/blog`)}>
               Blog
-           </div>
+            </div>
 
-           {/* <a className="hover:text-[#0fa0fa] text-[#555]" href="https://linkedin.com/company/paidre" target="_blank" rel="nofollow">
+            <a className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`https://linkedin.com/company/paidre`)} target="_blank" rel="nofollow">
               LinkedIn
-           </a>
-           <a className="hover:text-[#0fa0fa] text-[#555]" href="https://facebook.com/paidredotcom" target="_blank" rel="nofollow">
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`https://facebook.com/paidredotcom`)} target="_blank" rel="nofollow">
               Facebook
-           </a>
-           <a className="hover:text-[#0fa0fa] text-[#555]" href="https://paidre.betteruptime.com" target="_blank" rel="nofollow">
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`https://paidre.betteruptime.com`)} target="_blank" rel="nofollow">
               System status
-           </a> */}
+            </a>
           </div>
           <div className="mb-[16px] grid auto-rows-min gap-4">
             <h6 className="font-[600] mt-4">Legal</h6>
-           {/* <a className="hover:text-[#0fa0fa] text-[#555]" href={`${windowOrigin}/privacy-policy`} target="_blank" rel="nofollow" >
-              Privacy policy	
-           </a>
-           <a className="hover:text-[#0fa0fa] text-[#555]" href={`${windowOrigin}/terms-of-use`} target="_blank" rel="nofollow" >
+            <a className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/privacy-policy`)}>
+              Privacy policy
+            </a>
+            <a className="hover:text-[#0fa0fa] text-[#555] cursor-pointer" onClick={() => window.open(`${window?.location?.origin}/terms-of-use`)}>
               Terms of use
-           </a> */}
-          
-		  
+            </a>
           </div>
         </div>
 
